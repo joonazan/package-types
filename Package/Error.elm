a81344228e137371669dba4bc95b8ac6646eb01f
Joonatan Saarhelo <joon.saar@gmail.com>
2017-08-25 13:33:17 +0300

    add docs to error

--- Package/Error.elm
+++ Package/Error.elm
@@ -1,8 +1,14 @@
 module Package.Error exposing (..)
 
+{-| Just contains the error type
+@docs Error
+-}
+
 import Http
 
 
+{-| Encodes different errors that may occur fetching a module
+-}
 type Error
     = HttpError Http.Error
     | PackageNotFound
